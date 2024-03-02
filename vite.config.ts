94cbd11683d49c6815f16e2a47f90499efe3651c
damianamodeo <damianamodeotheocratic@gmail.com>
2024-03-02 20:54:40 +1100

    init 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,79 +1,75 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv } from 'vite';
-import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
-import htmlPlugin from 'vite-plugin-html-config';
-import Info from 'unplugin-info/vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+import htmlPlugin from "vite-plugin-html-config";
+import Info from "unplugin-info/vite";
 
-export default ({ mode } : { mode: string }) => {
+export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  const APP_NAME = process.env.VITE_APP_NAME || 'APP NAME';
+  const APP_NAME = process.env.VITE_APP_NAME || "APP NAME";
 
   const htmlPluginOpt = {
     title: APP_NAME,
     metas: [
       {
-        name: 'theme-color',
-        media: '(prefers-color-scheme: light)',
-        content: '#f7f7f7',
+        name: "theme-color",
+        media: "(prefers-color-scheme: light)",
+        content: "#f7f7f7",
       },
       {
-        name: 'theme-color',
-        media: '(prefers-color-scheme: dark)',
-        content: '#0d0d0d',
+        name: "theme-color",
+        media: "(prefers-color-scheme: dark)",
+        content: "#0d0d0d",
       },
       {
-        name: 'apple-mobile-web-app-title',
-        media: '-',
+        name: "apple-mobile-web-app-title",
+        media: "-",
         content: APP_NAME,
       },
     ],
   };
 
   const vitePWAOpt: Partial<VitePWAOptions> = {
-    registerType: 'autoUpdate',
+    registerType: "autoUpdate",
     manifest: {
       short_name: APP_NAME,
       name: APP_NAME,
       icons: [
         {
-          src: 'assets/manifest-icon-192.maskable.png',
-          sizes: '192x192',
-          type: 'image/png',
-          purpose: 'any',
+          src: "assets/manifest-icon-192.maskable.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "any",
         },
         {
-          src: 'assets/manifest-icon-192.maskable.png',
-          sizes: '192x192',
-          type: 'image/png',
-          purpose: 'maskable',
+          src: "assets/manifest-icon-192.maskable.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable",
         },
         {
-          src: 'assets/manifest-icon-512.maskable.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any',
+          src: "assets/manifest-icon-512.maskable.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any",
         },
         {
-          src: 'assets/manifest-icon-512.maskable.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'maskable',
+          src: "assets/manifest-icon-512.maskable.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
         },
       ],
-      start_url: '.',
-      display: 'standalone',
-      theme_color: '#ffffff',
-      background_color: '#ffffff',
+      start_url: ".",
+      display: "standalone",
+      theme_color: "#ffffff",
+      background_color: "#ffffff",
     },
   };
 
   return defineConfig({
-    plugins: [
-      htmlPlugin(htmlPluginOpt),
-      react(),
-      VitePWA(vitePWAOpt),
-      Info(),
-    ],
+    base: `/${process.env.VITE_APP_REPO_NAME}`,
+    plugins: [htmlPlugin(htmlPluginOpt), react(), VitePWA(vitePWAOpt), Info()],
   });
 };
